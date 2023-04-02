330f8ba5bd613c8f408b6a00cfb56d57c47a2286
2106027 <dasdiya0603@gmail.com>
2023-04-03 01:10:40 +0530

    first commit

---
+++ frontend/src/Components/Login/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function Form() {
+
+
+    return (
+        <form>
+            <div class="mb-3">
+                <label for="Email" class="form-label">Enter Email</label>
+                <input type="email" class="form-control" id="Email"/>
+            </div>
+        </form>
+    )
+}
--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import pic from './pic.jpg'
+import pic from './LoginPage.png'
+import Form from './Form.js'
 export default function Login() {
 
+  const Form = () =>{
+    <form>
+      <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+    </form>
+  }
   return (
-<div className='container card h-50 w-80' style={{marginTop:"10%"}}>
-  <div className="d-flex row g-0">
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title mx-5">Hi! Welcome Back!</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    <div className='container card h-50 w-80' style={{ marginTop: "10%" }}>
+      <div className="d-flex row g-0">
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title mx-5">Hi, Welcome Back!</h5>
+            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+            <Form/>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <img src={pic} className="img-fluid" />
+        </div>
       </div>
     </div>
-    <div className="col-md-4">
-      <img src={pic} className="img-fluid"/>
-    </div>
-  </div>
-</div>
   )
 }
