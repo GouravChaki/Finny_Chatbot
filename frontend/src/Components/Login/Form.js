a10bff894eddf2659c148bd8018cef853c477c33
2106027 <dasdiya0603@gmail.com>
2023-04-03 01:46:36 +0530

    Basic Login Page
    Have the basics of the Login page down. Still need to style to match design.

--- frontend/src/Components/Login/Form.js
+++ frontend/src/Components/Login/Form.js
@@ -6,8 +6,13 @@ export default function InputForm() {
         <form>
             <div class="mb-3">
                 <label for="Email" class="form-label">Enter Email</label>
-                <input type="email" class="form-control" id="Email"/>
+                <input type="email" class="form-control" id="Email" placeholder='Email'/>
             </div>
+            <div class="mb-3">
+                <label for="Pass" class="form-label">Enter Password</label>
+                <input type="password" class="form-control" id="Pass" placeholder='Password'/>
+            </div>
+            <button class="btn btn-primary" type="submit">Login</button>
         </form>
     )
 }
--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -2,20 +2,21 @@ import React from 'react'
 import pic from './LoginPage.png'
 import InputForm from './Form.js'
 export default function Login() {
-  
+
   return (
     <div className='container card h-50 w-80' style={{ marginTop: "10%" }}>
       <div className="d-flex row g-0">
-        <div className="col-md-8">
+        <div className="col-md-5">
           <div className="card-body">
             <h5 className="card-title mx-5">Hi, Welcome Back!</h5>
-            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+            <p className="card-text"><small className="text-body-secondary">Please put your login credentials below to start using the chatbot</small></p>
             <InputForm/>
           </div>
         </div>
-        <div className="col-md-4">
-          <img src={pic} className="img-fluid" />
-        </div>
+        <div className="col-5">
+        <div className="card">
+          <img src={pic} class="card-img" />
+        </div></div>
       </div>
     </div>
   )
