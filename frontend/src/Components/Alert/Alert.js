60ddaae566db5fc518253f69f027e9cfcce899d4
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-04-02 22:11:47 +0530

    Added frontend

--- frontend/src/Components/Alert/Alert.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Alert(props) {
-  return (
-    {a.alert &&<div className={`alert alert-${a.alert.color}`} role="alert">{a.alert.msg}</div>}
-  )
-}
--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,35 +1,21 @@
-import React, { useState, useEffect, useContext } from 'react';
-import MainContext from '../Context/MainContext'
+import React from 'react'
+import pic from './pic.jpg'
 export default function Login() {
-  const a = useContext(MainContext)
-  const change=()=>{
-    const obj = {
-      email: document.getElementById('ck_mail').value,
-      password: document.getElementById('ck_password').value
-    }
-    console.log(obj)
-    a.setUser(obj)
-  }
-  useEffect(()=>{
-        a.login();
-   },[a.user])
+
   return (
-    <>
-    {a.alert &&<div className={`alert alert-${a.alert.color}`} role="alert">{a.alert.msg}</div>}
-    <div className="container h-50 w-50 p-4 shadow mb-4 bg-body-tertiary rounded mt-5">
-    <h3 className="text-center mb-4"><b>LOGIN</b></h3>
-    <div className="input-group mb-3">
-  <span className="input-group-text" id="inputGroup-sizing-default" style={{backgroundColor:'#adb5bd'}}>Email</span>
-  <input type="email" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="xyz@gmail.com" id="ck_mail"/>
-   </div>
-<div className="input-group mb-3">
-  <span className="input-group-text" id="inputGroup-sizing-default" style={{backgroundColor:'rgb(211 195 23)'}}>Password</span>
-  <input type="password" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="ck_password"/>
-</div>
-<div className="text-center mt-5">
-<button type="submit" className="btn btn-dark mx-5" onClick={change}>Login</button>
-</div>
+<div className='container card h-50 w-80' style={{marginTop:"10%"}}>
+  <div className="d-flex row g-0">
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title mx-5">Hi! Welcome Back!</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div className="col-md-4">
+      <img src={pic} className="img-fluid"/>
+    </div>
   </div>
-  </>
+</div>
   )
 }
