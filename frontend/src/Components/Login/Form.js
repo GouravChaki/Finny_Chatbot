9fbaa8e97b9d1b870147d35a85ab46266f2f4c78
2106027 <dasdiya0603@gmail.com>
2023-04-03 01:23:27 +0530

    Changes to Login.js
    Made changes to Login.js to fix previous issue with two functions both named Form

--- frontend/src/Components/Login/Form.js
+++ frontend/src/Components/Login/Form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-export default function Form() {
+export default function InputForm() {
 
 
     return (
--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import pic from './LoginPage.png'
-import Form from './Form.js'
+import InputForm from './Form.js'
 export default function Login() {
-
-  const Form = () =>{
-    <form>
-      <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-    </form>
-  }
+  
   return (
     <div className='container card h-50 w-80' style={{ marginTop: "10%" }}>
       <div className="d-flex row g-0">
@@ -19,7 +10,7 @@ export default function Login() {
           <div className="card-body">
             <h5 className="card-title mx-5">Hi, Welcome Back!</h5>
             <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-            <Form/>
+            <InputForm/>
           </div>
         </div>
         <div className="col-md-4">
