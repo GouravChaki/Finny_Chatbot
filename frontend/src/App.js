828b6df42f619a84a67ef58ecd08da79beb28cba
2106027 <dasdiya0603@gmail.com>
2023-04-04 22:16:43 +0530

    Navbar
    First commit for the Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
+import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <>
+    <Navbar/>
     <Login />
     {/* <Signup/> */}
     </>
