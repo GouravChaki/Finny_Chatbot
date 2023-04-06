3bfd71f2e4acd6435d90cec3184611da38e1948e
MDas08 <dasdiya0603@gmail.com>
2023-04-07 00:20:32 +0530

    changed chatbot dimensions
    the chatbot now takes up most of the screen and is responsive even on phone screen

--- frontend/src/Components/Chatbot/ChatPage.js
+++ frontend/src/Components/Chatbot/ChatPage.js
@@ -1,12 +1,13 @@
 import Chatbot from 'react-chatbot-kit'
 import 'react-chatbot-kit/build/main.css'
+import './ChatPage.css'
 import config from './config.js';
 import MessageParser from './MessageParser.jsx';
 import ActionProvider from './ActionProvider.jsx';
 
 export default function ChatPage() {
     return (
-        <div>
+        <div className='d-flex align-items-center justify-content-center w-100 h-auto'>
             <Chatbot
                 config={config}
                 messageParser={MessageParser}
