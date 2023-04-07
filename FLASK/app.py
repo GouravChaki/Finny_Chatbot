f4afbdcb2152ef2e1261e1af51274381d4aff851
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-04-07 23:27:37 +0530

    Flask-Feature:1 completed

--- FLASK/app.py
+++ FLASK/app.py
@@ -4,35 +4,59 @@ import yfinance as yf #importing yahoo finance api
 # from train import dance
 from return_model import ml_model
 import pandas as pd
-app=Flask(__name__)
+from datetime import date
 from companies_list import companies
+from predict_price import predict_data
+
+app=Flask(__name__)
 
 @app.route('/yours_price',methods=['GET','POST'])
 async def hello():
   if (request.method=='GET'):
-    name=request.get_json()
-    # print(name['data'])
-    #getting dataset according to company nasdaq code
-    dataset = yf.Ticker(name['data'])
-    # print(dataset.info)
     
-    # data history for that particular company
-    hist = dataset.history(period="2mo") 
-    print(hist)
-    df=pd.DataFrame(hist)# converting the datahistory to dataframe
+    price=request.get_json()#fetch price
+    price=float(price['price'])
 
-    df.to_csv('temporary_store_data.csv') # to store data temporarily in temporary_store_data.csv file
-    df_read=pd.read_csv('temporary_store_data.csv')# to read from temporary_store_data.csv
+    companies_df=await companies(price)#get company list falling in this price
+    companies_df['price_5days']=0
+    companies_df['price_7days']=0
+    companies_df['price_10days']=0
     
-    # print(df_read)
+    today = date.today()
+    date_current=int(today.strftime("%Y%m%d")) #current date
+    # print(type([price]))
+    for i in (companies_df.index):
+       name=companies_df['Symbol'][i]
+       price0,price1,price2,price3=await predict_data(name,date_current)
+       if (price0!="empty"):
+          companies_df.loc[i,'Last Sale']=price0
+          companies_df.loc[i,'price_5days']=price1
+          companies_df.loc[i,'price_7days']=price2
+          companies_df.loc[i,'price_10days']=price3
     
-    model=await ml_model(df_read)
-    p=model.predict([[20230405]])
-    print(p)
-    price=(p[0][0]+p[0][1]+p[0][2]+p[0][3])/4
-    print(type([price]))
+    print(companies_df)
+    if(price>=10000.0):
+        companies_df=companies_df[(companies_df['price_10days']>=price)]
+    
+    elif(price>=5000.0 and price<=10000.0):
+        companies_df=companies_df[(companies_df['price_10days']>=price) & (companies_df['Last Sale']<=10000)]
+    
+    elif(price>=3000.0 and price<=5000.0):
+        companies_df=companies_df[(companies_df['price_10days']>=price) & (companies_df['Last Sale']<=5000)]
+  
+    elif(price>=2000.0 and price<=3000.0):
+        companies_df=companies_df[(companies_df['price_10days']>=price) & (companies_df['Last Sale']<=3000)]
+        
+    elif(price>=1000.0 and price<=2000.0):
+        companies_df=companies_df[(companies_df['price_10days']>=price) & (companies_df['Last Sale']<=2000)]
+       
+    else:
+       companies_df=companies_df[(companies_df['price_10days']>=(price-0.05*price)) & (companies_df['price_10days']<=(price+0.05*price))]
+    
+    print(companies_df)
+    companies_df=companies_df.to_json()
 
-    return [price]
+    return companies_df
     # return 'hello'
 
 @app.route('/realtime',methods=['GET','POST'])
--- FLASK/companies_list.py
+++ FLASK/companies_list.py
@@ -8,6 +8,22 @@ async def companies(price):
     df=pd.read_csv('nasdaq_use.csv')
 
     #sort and return values based upon a range
-    symbol=df[(df['Last Sale']>=(price-0.004*price)) & (df['Last Sale']<=(price+0.004*price))]
+    if(price>=10000.0):
+        symbol_df=df[df['Last Sale']>=price]
     
-    return symbol
+    elif(price>=5000.0 and price<=10000.0):
+        symbol_df=df[(df['Last Sale']>=price) & (df['Last Sale']<=10000)]
+    
+    elif(price>=3000.0 and price<=5000.0):
+        symbol_df=df[(df['Last Sale']>=price) & (df['Last Sale']<=5000)]
+    
+    elif(price>=2000.0 and price<=3000.0):
+        symbol_df=df[(df['Last Sale']>=price) & (df['Last Sale']<=3000)]
+    
+    elif(price>=1000.0 and price<=2000.0):
+        symbol_df=df[(df['Last Sale']>=price) & (df['Last Sale']<=2000)]    
+    
+    else:
+        symbol_df=df[(df['Last Sale']>=(price-0.003*price)) & (df['Last Sale']<=(price+0.003*price))]
+    
+    return symbol_df
