041e17d672b775afba613ce32c7175f2cbad58d2
MDas08 <dasdiya0603@gmail.com>
2023-04-08 22:42:42 +0530

    changed chatbot messages
    changed chatbot messages and extracted data from messages

--- frontend/src/Components/Chatbot/MessageParser.js
+++ frontend/src/Components/Chatbot/MessageParser.js
@@ -1,30 +1,83 @@
 import React from 'react';
-import {useContext} from 'react';
+import { useContext } from 'react';
 import Context from '../Context_api/Context'
 const MessageParser = ({ children, actions }) => {
-  const a=useContext(Context)
+  const a = useContext(Context)
   // var re = new RegExp("/([1-9][0-9]*)/");
   const parse = (message) => {
+
     const lowerCaseMessage = message.toLowerCase();
-    if (lowerCaseMessage.includes('hello')) {
-        actions.handleHello();
+    var code;
+    if (lowerCaseMessage.includes('budget')) {
+      //recommend stock on user budget
+      try{
+      var num = lowerCaseMessage.match(/\d+\.?\d*/)[0];
+      num = parseFloat(num); //num extracted to be sent to back-end
+      actions.recommend1();
+      }
+      catch(e)
+      {
+        actions.wrongInput();
+      }
+      
     }
-  if (lowerCaseMessage.includes('nasdaq1')) {
-    //recommend stock on future price
-       actions.recommend2();
-   }
-   if (lowerCaseMessage.includes('nasdaq2')) {
-    //find future price of a stock
-    actions.recommend2();
-}
-if (lowerCaseMessage.includes('nasdaq3')) {
-  //historical data of a company
-  actions.showData();
-}
-if (lowerCaseMessage.includes('budget')) {
-  //recommend stock on user budget
-     actions.recommend1();
- }
+
+    if (lowerCaseMessage.includes('hello')) {
+      actions.handleHello();
+    }
+
+    if (lowerCaseMessage.includes('nasdaq1')) {
+      //recommend stock on future price
+      try{
+        lowerCaseMessage = lowerCaseMessage.replace("nasdaq1", '');
+      code = lowerCaseMessage.replace(/[^A-Za-z' ']/g,'');
+      lowerCaseMessage = lowerCaseMessage.replace(",", ' ');
+      code = code.toUpperCase(); //NASDAQ Codes Extracted from message
+      var num = lowerCaseMessage.match(/\d+\.?\d*/)[0];
+      num = parseInt(num); //no. of days
+      actions.recommend2();
+      }
+      catch(e)
+      {
+        actions.wrongInput();
+      }
+      
+    }
+
+    if (lowerCaseMessage.includes('nasdaq2')) {
+      //find future price of a stock
+      try{
+        lowerCaseMessage = lowerCaseMessage.replace("nasdaq2", '');
+      code = lowerCaseMessage.replace(/[^A-Za-z]/g,'');
+      code = code.toUpperCase(); //NASDAQ Code Extracted from message
+      var num = lowerCaseMessage.match(/\d+\.?\d*/)[0];
+      num = parseInt(num); //no. of days
+      actions.recommend2();
+      }
+      catch(e)
+      {
+        actions.wrongInput();
+      }
+      
+    }
+
+    if (lowerCaseMessage.includes('nasdaq3')) {
+      //historical data of a company
+      try{
+        lowerCaseMessage = lowerCaseMessage.replace("nasdaq3", '');
+      code = lowerCaseMessage.replace(/[^A-Za-z]/g,'');
+      code = code.toUpperCase(); //NASDAQ Code Extracted from message
+      var date = lowerCaseMessage.match('[0-9]{4}([\-/ ])[0-9]{2}[\-/ ][0-9]{2}')[0];//date extracted as string
+        actions.showData();
+      }
+      catch(e)
+      {
+        actions.wrongInput();
+      }
+      
+      
+    }
+    
   };
 
   return (
