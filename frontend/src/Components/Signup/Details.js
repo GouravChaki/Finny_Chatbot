e056a39584e84dbc2c07b4d94058839763c7c30a
Gargee-Bhowmick <gargee.bhowmick09@gmail.com>
2023-04-09 15:05:38 +0530

    Compilation done

--- frontend/src/Components/Signup/Details.js
+++ frontend/src/Components/Signup/Details.js
@@ -3,7 +3,17 @@ import './Details.css'
 import Alert from './Alert'
 export default function Details() {
   const [value,setValue]=useState()
-  const alert=()=>{
+  const alert=(event)=>{
+    const a = useContext(Context)
+      event.preventDefault()
+      const obj={
+         firstname: document.getElementById("firstname").value,
+         lastname: document.getElementById("lastname").value,
+         email: document.getElementById("email").value,
+         password: document.getElementById("password").value
+      }
+      console.log(obj)
+      a.Signup(obj)
     setValue(1)
   }
 return (
@@ -11,20 +21,20 @@ return (
 <form> 
 <div className="row">
   <div className="col">
-    <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+    <input type="text" className="form-control" name="firstname" id="firstname" placeholder="First name" aria-label="First name"/>
   </div>
   <div className="col">
-    <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
+    <input type="text" className="form-control" name="lastname" id="lastname" placeholder="Last name" aria-label="Last name" />
   </div>
-<div className='mail'>
-  <input type="email" className="form-control"  placeholder="Email" />
+<div >
+  <input type="email" className="form-control" name="email" id="email" placeholder="Email" />
   </div>
-<div className='pass'>
-  <input type="password" className="form-control"  placeholder="Password" />
+<div>
+  <input type="password" className="form-control" name="password" id="password" placeholder="Password" />
 </div>
 </div>
 <div className="d-grid gap-2 col-6 mx-auto">
-  <button className="btn btn-primary" id="signupbt" type="button" onClick={alert}>Sign Up</button>
+  <button className="btn btn-primary" id="signupbt" type="button" onClick={alert} >Sign Up</button>
 </div>
 {value && <Alert value={value} setValue={setValue}/>}
 </form>
