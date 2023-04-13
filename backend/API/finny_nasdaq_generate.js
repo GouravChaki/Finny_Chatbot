fae4f60baa0947da9fc92b13df5b0031193c252d
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-04-14 00:51:46 +0530

    Delete backend directory

--- backend/API/finny_nasdaq_generate.js
+++
@@ -1,7 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
-
-let json = csvToJson.getJsonFromCsv("./API/nasdaq.csv")
-const finny_nasdaq_api=()=>{
-    console.log(json)
-}
-module.exports=finny_nasdaq_api
--- backend/Readme.txt
+++
@@ -1,3 +0,0 @@
----TO CONVERT CSV TO JSON----
-
-$ npm install convert-csv-to-json --save
